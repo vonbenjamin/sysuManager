a7922bb7751105302bfa847bed96f879a32c970a
benjamin <673010729@qq.com>
2018-05-09 12:13:23 +0800

    sql修改

--- 源码及部署说明文件/sql/sql.sql
+++ 源码及部署说明文件/sql/sql.sql
@@ -13,14 +13,20 @@ File Encoding         : 65001
 Date: 2016-12-02 10:44:34
 */
 
+
 -- ----------------------------
 -- Table structure for `user`
 -- ----------------------------
-
 CREATE TABLE `user` (
   `id` int(10) NOT NULL,
   `username` text NOT NULL,
   `password` varchar(255) NOT NULL,
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+-- ----------------------------
+-- Records of journal_info
+-- ----------------------------
+INSERT INTO `user`(`id`, `username`, `password`, `name`) VALUES (1, '15332014', '1234', 'student');
+INSERT INTO `user`(`id`, `username`, `password`, `name`) VALUES (2, '15332015', '12345', 'student');
+INSERT INTO `user`(`id`, `username`, `password`, `name`) VALUES (3, '1234567', '123456', 'teacher');
